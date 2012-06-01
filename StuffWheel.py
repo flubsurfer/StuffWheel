f4928d6a31ce65717f72b81a5a4cce52ccf47235
Rusel Bruff <ruseljr@gmail.com>
2012-06-01 00:07:22 -0400

    first commit

---
+++ StuffWheel.py
@@ -0,0 +1,83 @@
+import random
+import time
+
+
+Comedy_Movies = ['Borat', 'Anchorman', 'Team_America', 'Napoleon_Dynamite', 'Think_Like_A_Man', 
+'Grandmas_Boy', 'Office Space', 'Coming_to_America', 'Big_Lebowski', 'The_Waterboy', 
+'Friday', 'Swingers', 'The_Three_Stooges', 'Wedding_Crashers', 'Theres_Something_About_Mary']
+Scary_Movies = ['Hills Have Eyes', 'It', 'Lepercaun', 'Silence of the Lambs']
+Drama_Movies = ['Boiler_Room', 'Willy Wonka and the Chocolate Factory', 'Shawshank Redemption', 'Limitless']
+Documentaries = ['House of Saud']
+SciFi = []
+Standup_Comedy = []
+Comedy_Shows = []
+Scary_Shows = []
+Drama_Shows = []
+Reality_Shows = []
+Beauty_Vlogs = []
+Fashion_Vlogs = []
+Culture_Vlogs = []
+Reviews = []
+Comedy_Webisodes = []
+Video_Political_Podcasts = []
+Video_Sports_Podcasts = []
+Video_News_Podcasts= []
+Comedy_Webisodes = []
+
+Audio_Political_Podcasts = []
+Audio_Sports_Podcasts = []
+Audio_News_Podcasts = []
+
+
+
+
+
+
+Movies = [Comedy_Movies, Scary_Movies, Drama_Movies, Documentaries, Standup_Comedy, SciFi]
+Shows =  [Comedy_Shows, Scary_Shows, Drama_Shows, Reality_Shows]
+Vlogs = [Beauty Vlogs, Fashion Vlogs, Culture Vlogs, Reviews]
+Webisodes = ['Comedy Webisodes',]
+Video_Podcasts = ['Politics', 'Sports', 'News']
+
+Audio_Podcasts = ['Politics', 'Sports', 'News']
+#Songs = []
+
+Photos = ['Beauty', 'Architecture', 'Design']
+Memes = []
+Arts = []
+
+
+Articles = ['Politics', 'Sports', 'News']
+ebooks = ['Politics', 'Sports', 'News']
+
+
+
+Texts = [Articles, ebooks]
+Pictures = [Photos, Memes, Arts]
+Audio = ['Podcasts', 'Songs']
+Video = [Movies, Shows, Vlogs, Webisodes]
+Apps = ['Productive', 'Games', 'Dating', 'Business', 'Utilities']
+
+
+"""
+question = raw_input("What would you like to do? Read? Listen? Look at pictures? Watch a video? Use an app? ")
+def response():
+	if question == Read:
+		response = raw_input("Articles or eBooks? ")
+	if question == Listen:
+		response = raw_input("Articles or eBooks? ")
+	if question == Look at pictures:
+		response = raw_input("Articles or eBooks? ")
+	if question == Watch a video:
+		response = raw_input("Articles or eBooks? ")
+	if question == Use an app
+		response = raw_input("Articles or eBooks? ")
+	
+"""
+
+Comedy_Movie = random.choice(Comedy_Movies)
+Movie = random.choice(Movies)
+
+print Comedy_Movie
+	
+	
